c6a67d61ac4ce1858443b73ceff620f4604b0021
Your Name <s.nguyen11@utexas.edu>
2017-03-22 18:40:06 -0500

    added questions array

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+var questions = [
+	{question: "What is the biggest species of shark?",
+		correctAnswer: "Whale shark",
+		answers: ["Whale shark",  "Great white shark", "Hammerhead shark", "Basking shark"]},
+
+	{question: "What is the name for a baby horse?",
+		correctAnswer: "Foal",
+		answers: ["Foal", "Calf", "Cub", "Cygnet"]
+	},
+	{question: "A seahorse is a(n) _____.",
+			correctAnswer: "Fish",
+			answers: ["Fish", "Crustacean", "Mammal", "Arachnid"]
+	},
+	{question: "Which of these birds has the largest wingspan?",
+		correctAnswer: "Albatross",
+		answers: ["Albatross", "Stork", "Swan", "Condor"]
+	},
+	{question: "What is the biggest animal that has ever lived?",
+		correctAnswer: "Blue whale",
+		answers: ["Blue whale", "African elephant", "Brontosaurus", "Spinosaurus"]
+	},
+	{question: "Which is the fastest flying bird?",
+		correctAnswer: "Peregrine falcon",
+		answers: ["Peregrine falcon", "Harpy eagle", "Grey-headed albatross", "Common swift"]
+	},	
+	{question: "What are female elephants called?",
+		correctAnswer: "Cows",
+		answers: ["Cows", "Mares", "Sows", "Dames"]
+	},
+	{question: "What is the fastest water animal?",
+		correctAnswer: "Sailfish",
+		answers: ["Sailfish", "Porpoise", "Marlin", "Barracuda"]
+	},
+	{question: "What is a group of frogs called?",
+		correctAnswer: "Army",
+		answers: ["Army", "Herd", "Flock", "Troop"]
+	},	
+	{question: "Which of these mammals lays eggs?",
+		correctAnswer: "Echidna",
+		answers: ["Echidna", "Bongo", "Kiwi", "Southern right whale"]
+	}				
+];
+
+
+});
+
